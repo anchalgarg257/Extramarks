2ac3f8640c89ac043902403d4d8dc9cc868ff5d6
Anchal <anchalgarg257@gmail.com>
2020-01-12 13:27:55 +0530

    New code committed.

--- src/main/java/org/example/ListofEmployees.java
+++ src/main/java/org/example/ListofEmployees.java
@@ -1,30 +1,31 @@
 package org.example;
 
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
 public class ListofEmployees {
 
     public static void main(String[] args) {
 
-        final String CSV_LOCATION = "Employee.csv ";
+        final String CSV_LOCATION = "D:/Path/Employee.csv";
 
         try {
 
             // Creating writer class to generate
             // csv file
-            FileWriter writer = new FileWriter("D:/Path");
+            FileWriter writer = new FileWriter(CSV_LOCATION);
 
             System.out.println("Enter the details of 2 Employees");
             Scanner scanner = new Scanner(System.in);
 
-            List<EmployeeBean> list = new ArrayList<>();
+            List<EmployeeBean> list = new ArrayList();
             for (int i = 1; i <= 2; i++) {
 
                 EmployeeBean e1 = new EmployeeBean();
@@ -45,31 +46,40 @@ public class ListofEmployees {
                 e1.setDepartment(scanner.next());
 
                 list.add(e1);
+            }
 
                 // Create Mapping Strategy to arrange the
                 // column name in order
                 ColumnPositionMappingStrategy mappingStrategy=
                         new ColumnPositionMappingStrategy();
-                mappingStrategy.setType(Employee.class);
+                mappingStrategy.setType(EmployeeBean.class);
 
                 // Arrange column name as provided in below array.
                 String[] columns = new String[]
-                        { "Name", "Age", "Company", "Salary", "Department"};
+                        {"Id", "Name", "Salary", "Location", "Department"};
                 mappingStrategy.setColumnMapping(columns);
 
                 // Createing StatefulBeanToCsv object
-                StatefulBeanToCsvBuilder<Employee> builder=
+                StatefulBeanToCsvBuilder<EmployeeBean> builder=
                         new StatefulBeanToCsvBuilder(writer);
                 StatefulBeanToCsv beanWriter =
                         builder.withMappingStrategy(mappingStrategy).build();
 
                 // Write list to StatefulBeanToCsv object
-                beanWriter.write(EmployeeList);
+                beanWriter.write(list);
 
                 // closing the writer object
                 writer.close();
-            }
+                System.out.println("Your data has been saved successfully.");
+
+        } catch (IOException ex) {
+            System.out.println("Your data has not been saved.");
+            ex.printStackTrace();
         }
+        catch (Exception ex) {
+            System.out.println("Your data has not been saved.");
+        ex.printStackTrace();
+    }
 //        System.out.println("The details of the employees are as follows:");
 //        System.out.println("Employee ID" + "\t" + "Employee name" + "\t" + "Employee Salary" + "" +
 //                "\t" + "Company Location" + "\t" + "Department");
@@ -78,8 +88,5 @@ public class ListofEmployees {
 //                    employeeBean.getDepartment() + "\t" + employeeBean.getLocation());
 //        }
 
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-}
